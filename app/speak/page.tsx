f4c332a1c706ca6e061b7ef789b568c34a99c827
mohikbalabdkasim02-cyber <mohikbalabdkasim.02@gmail.com>
2026-09-17 22:08:41 +0800

    feat: add speaking practice page

---
+++ app/speak/page.tsx
@@ -0,0 +1,5 @@
+import SpeakingRecorder from '@/components/recorder/SpeakingRecorder'
+
+export default function SpeakPage(){
+ return <main className="p-8"><h1 className="text-3xl font-bold">Speak Practice</h1><SpeakingRecorder/></main>
+}
