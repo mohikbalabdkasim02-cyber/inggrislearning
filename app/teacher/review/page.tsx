0c24d366f6c30a8c5b9b56f9c6b9e16d5be54ec3
mohikbalabdkasim02-cyber <mohikbalabdkasim.02@gmail.com>
2026-09-17 22:10:34 +0800

    feat: add teacher review page

---
+++ app/teacher/review/page.tsx
@@ -0,0 +1,3 @@
+import FeedbackCard from '@/components/feedback/FeedbackCard'
+
+export default function ReviewPage(){return <main className="p-8"><h1 className="text-3xl font-bold">Review Speaking</h1><FeedbackCard/></main>}
